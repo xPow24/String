de765f8af07b925f3711d0e9892989182e5a25d8
xPow24 <114853668+xPow24@users.noreply.github.com>
2022-10-29 16:01:30 +0700

    Update SuffixArray.cpp

--- SuffixArray.cpp
+++ SuffixArray.cpp
@@ -1,40 +1,36 @@
 #include<bits/stdc++.h>
-#define REP(i, n) for (int i = 0; i < (int)(n); ++i)
 using namespace std;
 const int MAXN = 1 << 21;
 int N, gap;
 int sa[MAXN], pos[MAXN], tmp[MAXN], lcp[MAXN];
 
-bool sufCmp(int i, int j) {
+bool suffcmp(int i, int j) {
   if (pos[i] != pos[j]) return pos[i] < pos[j];
   i += gap;
   j += gap;
   return (i < N && j < N) ? pos[i] < pos[j] : i > j;
 }
 
-void buildSA(string SS) {
-  N = SS.size();
-  REP(i, N) {
-    sa[i] = i; 
-    pos[i] = SS[i];
+void buildSA(string ss) {
+  for (int i = 0; i < N; ++i) {
+    sa[i] = i;
+    pos[i] = ss[i];
   }
-  for (gap = 1;; gap *= 2) {             
-    sort(sa, sa + N, sufCmp);
-    REP(i, N - 1) {
-      tmp[i + 1] = tmp[i] + sufCmp(sa[i], sa[i + 1]);
-    }
-    REP(i, N) pos[sa[i]] = tmp[i];
-    if (tmp[N - 1] == N - 1) break; 
+  for (gap = 1;; gap *= 2) {
+    sort(sa, sa + N, suffcmp);
+    for (int i = 0; i < N - 1; ++i) tmp[i + 1] = tmp[i] + suffcmp(sa[i], sa[i + 1]);
+    for (int i = 0; i < N; ++i) pos[sa[i]] = tmp[i];
+    if (tmp[N - 1] == N - 1) break;
   }
 }
 
-void buildLCP(string SS) {
+void buildLCP(string ss) {
   lcp[0] = 0;
   for (int i = 0, k = 0; i < N; ++i) {
     if (pos[i]) {
-      for (int j = sa[pos[i] - 1]; SS[i + k] == SS[j + k];)
-      ++k;
-      lcp[pos[i]] = k;
+      int j = sa[pos[i] - 1];
+      while (ss[i + k] == ss[j + k]) ++k;
+      lcp[pos[i]] = k; 
       if (k) --k;
     }
   }
@@ -43,7 +39,10 @@ void buildLCP(string SS) {
 int main() {
   string s;
   cin >> s;
+  N = s.size();
   buildSA(s); buildLCP(s);
-  REP(i, N) cout << lcp[i] << ' ';
+  for (int i = 0; i < N; ++i) cout << sa[i] << ' ';
+  cout << '\n';
+  for (int i = 0; i < N; ++i) cout << lcp[i] << ' ';
   return 0;
 }
