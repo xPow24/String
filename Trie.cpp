04899c46371dbc5b7d35e0e4b072f434f2d4f5f8
Lê Trọng Bảo <114853668+xPow24@users.noreply.github.com>
2022-10-30 09:59:40 +0700

    Create Trie.cpp

---
+++ Trie.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int MAXN = 5*1e5;
+int trie[MAXN][26], finish[MAXN], node;
+void buildTrie(string s) {
+  int cur = 0, n = s.size();
+  for (int i = n - 1; i >= 0; --i) {
+    int tt = s[i] - 'a';
+    if (trie[cur][tt] == 0) trie[cur][tt] = ++node;
+    cur = trie[cur][tt];
+  }
+  finish[cur]++;
+}
+int main() {
+  int n;
+  string ss;
+  cin >> n;
+  while (n--) {
+    cin >> ss;
+    buildTrie(ss);
+  }
+}
